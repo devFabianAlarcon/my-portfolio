3371ee527cb15ae14ae2b1ff1a2c8bbed555c2a5
Fabian Alarcon <fgalarconc@gmail.com>
2024-04-22 17:55:08 -0500

    :iphone:  Mobile font

--- src/utils.js
+++ src/utils.js
@@ -15,7 +15,7 @@ export function displayDialogue(text, onDisplayEnd) {
         }
 
         clearInterval(intervalRef);
-    }, 5);
+    }, 10);
 
     const closeBtn = document.getElementById("close");
 
