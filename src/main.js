86160dc8630fb6e1be1a74fd068b51d793d2dcc1
Fabian Alarcon <fgalarconc@gmail.com>
2024-04-22 15:42:00 -0500

    :video_game: Player movement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx"
-import { displayDialogue } from "./utils";
+import { displayDialogue, setCamScale } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
@@ -79,6 +79,12 @@ k.scene("main", async () => {
         }
     }
 
+    setCamScale(k);
+
+    k.onResize(() => {
+        setCamScale(k);
+    })
+
     k.onUpdate(()=> {
         k.camPos(player.pos.x, player.pos.y + 100);
     });
@@ -87,6 +93,53 @@ k.scene("main", async () => {
 
         const worldMousePos = k.toWorld(k.mousePos());
         player.moveTo(worldMousePos, player.speed);
+
+        const mouseAngle = player.pos.angle(worldMousePos)
+
+        const lowerBound = 50
+        const upperBound = 125;
+
+        // Player animacion arriba
+        if (mouseAngle > lowerBound && mouseAngle < upperBound && player.curAnim() !== "walk-up") {
+            player.play("walk-up");
+            player.direction = "up";
+            return;
+        }
+
+        // Player animacion abajo
+        if (mouseAngle < -lowerBound && mouseAngle > -upperBound && player.curAnim() !== "walk-down") {
+            player.play("walk-down");
+            player.direction = "down";
+            return;
+        }
+
+        // Player animacion lados
+        if (Math.abs(mouseAngle) > upperBound) {
+            player.flipX = false;
+            if (player.curAnim() !== "walk-slide") player.play("walk-slide");
+            player.direction = "right";
+            return;
+        }
+
+        if (Math.abs(mouseAngle) < lowerBound) {
+            player.flipX = true;
+            if (player.curAnim() !== "walk-slide") player.play("walk-slide");
+            player.direction = "left";
+            return;
+        }
+    })
+
+    k.onMouseRelease(() => {
+        if (player.direction === "down") {
+            player.play("idle-down");
+            return;
+        }
+        if (player.direction === "up") {
+            player.play("idle-up");
+            return;
+        }
+
+        player.play("idle-slide");
     })
 });
 
