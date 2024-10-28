ab8a80c31f8fb99175eec91f73176bfd3582b2d5
Fabian Alarcon <fgalarconc@gmail.com>
2024-10-28 12:26:41 -0500

    :rocket: Code Lover Innovation

--- src/constants.js
+++ src/constants.js
@@ -1,57 +1,41 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  "welcome": `Hola soy <strong>Fabián Alarcon</strong>, Bienvenid@ a la oficina... 
-  <br><br>
-  A continuación te voy a contar un poco de mí, asi como de mi proceso en el programa <strong style="color: red;">Tech Camp</strong>, te invito a interactuar con cada uno de los objetos que encuentres, para que descubras lo maravilloso que ha sido participar en este programa. <br><br> 
+  "welcome": `
+  <h2>¡Bienvenido!</h2>
+  Acompáñanos a conocer a <strong>Fabián</strong> un TechCamper que encontramos dentro de la caja. <br><br> 
   <div style="text-align: center;"><img src="./davivienda.png" alt="Mi imagen" style="max-width: 8rem;"></div>`,
   "davivienda":`     
-    <h2>Quién es Fabián?</h2>
-    <h3>Una breve introducción</h3>
+    <h2>¿Quién es Fabián?</h2>
     <br>
-    <div>
-      <ul>
-        <li><strong>Nombre:</strong> Fabián</li>
-        <li><strong>Edad:</strong> 26 años</li>
-        <li><strong>Residencia:</strong> Manizales</li>
-        <li><strong>Profesión:</strong> Ingeniero de Software</li>
-        <li><strong>Pasión:</strong> Desarrollo de software</li>
-        <li><strong>Hobbies:</strong> Viajar, Video juegos, Aprender</li>
-      </ul>
-    </div>`,
+    Fabian era Secretario de Gerencia de la Sucursal Manizales, pero su sueño era ejercer su profesión como Ingeniero de software en el mundo de la tecnología y el desarrollo.`,
     "cambios":`     
-    <h2>Cambios y aprendizajes</h2>
-    <h3>Mi transformación personal</h3>
+    <h2>El debate interno</h2>
     <br>
+    Por otro lado, el negocio se cuestionaba…
     <div class="content">
         <ul>
-            <li>Valorar la colaboración y el compañerismo</li>
-            <li>Ser creativo y adaptable a los cambios</li>
-            <li>Llenarse de pasión y sueños por convertirse en un líder tecnológico</li>
-            <li>Adquirir conocimientos técnicos y de equipo en DevOps, DevSecOps, IaC con Terraform, AWS, Script en Bash, Pipelines en Github Actions, CI/CD, entre otros.</li>
-            <li>Aprender una amplia gama de lenguajes de programación como NodeJS, Java, Python, TypeScript, Angular.</li>
-            <li>Practicar principios fundamentales dentro del desarrollo de software ágil como Yagni, Kiss, Clean Code, BoyScout, algunos Patrones de Diseño de software y Scrum.</li>
+            <li>¿Y si no tiene experiencia, cuánto tiempo se va a tardar en aprender y esto cuánto nos va a costar?</li>
+            <li>¿Quién lo va a reemplazar?</li>
+            <li>¿Cuáles son las ventajas y desventajas de traer a una persona externa con experiencia?</li>
         </ul>
     </div>`,
     "orgullo":`     
-    <h2>Logros y contribuciones</h2>
-    <h3>Destacando mis hitos</h3>
+    <h2>Code Lover + Davivienda Lover = Resultados excepcionales</h2>
     <br>
+    Nosotros sabíamos que para ser un Coder Lover solo se necesitaba pasión. 
+    <br> Lo encontramos dentro de la caja y con un valor agregado.. ser un Davivienda Lover, impregnado de nuestro fit cultural.
+    <br><br> <strong>¿Y cómo lo hicimos?...</strong>`,
+    "impacto":`     
+    <h2>Buscando Oportunidades</h2>
+    <br>
+    Había golpeado puertas, pero le surgían varios cuestionamientos…
     <div class="content">
         <ul>
-            <li>Creación de nuevo módulo OpenID Connect (OIDC) en IaC con Terraform para AWS.</li>
-            <li>Creación de regla AWS mediante event bridge en IaC en Terraform, para optimización de costos en programación de horarios para encender y apagar clusters en horas no laborales.</li>
-            <li>Implementación y configuración de IaC de componentes para el proyecto Simulator Mngr, desplegando componentes como Bucket S3, Dynamo DB, Servicio ECS, Secrets manager para la autenticación con Data Power, API Gateway.</li>
-            <li>Actualización de módulos de IaC como Aws Secrets Manager con implementación rotation function y Aws Lambda con implementación de lambda@edge.</li>
-            <li>Implementación de configuración de networking para la comunicación entre servicios ECS de Aws a través de Service Connect de AWS con Terraform.</li>
-            <li>Generación de componentes en distintos proyectos como Harmony y Quality Manager.</li>
-            <li>Actualización y Generación de Documentación en Confluence para los módulos: Aws Lambda, Aws Cloudfront, Aws S3, Aws SQS, Aws ECS.</li>
-            <li>Participación en el desarrollo en Framework de Producto, apoyando en la elaboración de Test unitarios, optimización de código con EsLint.</li>
-            <li>Mini Dojos en Terraform, JPA, MapStruct, Lombok, ExpressJs, Patrones de Diseño de software y Manejo de excepciones en Java.</li>
+            <li>¿Cómo puedo poner a disposición de la organización mis talentos, para crecer y ser visible desde mi Sucursal en el Eje Cafetero?</li>
+            <li>Veo muy lejanas mis posibilidades de crecimiento, porque debo esperar a que alguien se pensione o se retire de la organización.</li>
+            <li>Las oportunidades que quiero están en Bogotá y no tengo experiencia en esas áreas.</li>
         </ul>
-    </div>`,
-    "impacto":`     
-    <h2>Impacto y futuro</h2>
-    <h3>Mirando hacia adelante</h3>`  
+    </div>`  
 };
   
